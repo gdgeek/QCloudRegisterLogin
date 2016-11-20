89fc6879e69b92d04dd9936ae456cafb5f4979bf
gdgeek <nethz@163.com>
2016-11-20 13:52:08 +0800

    更改成网上数据库

--- Config/DbPdo.php
+++ Config/DbPdo.php
@@ -15,9 +15,14 @@ class DbPdo{
     public $db;
 
     function __construct() {
-        $server = "localhost:8889";
-        $user = "web";
-        $pwd = "Ot6QzOCnYEhQl4Y2";
+       // $server = "localhost:8889";
+
+        $server = "583133f034b53.gz.cdb.myqcloud.com:15569";
+
+        $user = "cdb_outerroot";
+        $pwd = "qs^%$#@!";
+        //$user = "web";
+        //$pwd = "Ot6QzOCnYEhQl4Y2";
         $db = "qgame";
         try{
             $this->db = new \PDO("mysql:host=$server;dbname=$db", $user, $pwd);
