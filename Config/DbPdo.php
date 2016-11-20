df6480dd2215840b06ebbe7e5d8e2bebbc84d415
gdgeek <nethz@163.com>
2016-11-20 14:49:58 +0800

    eee

--- Config/DbPdo.php
+++ Config/DbPdo.php
@@ -17,15 +17,15 @@ class DbPdo{
     function __construct() {
        // $server = "localhost:8889";
 
-        $server = "583133f034b53.gz.cdb.myqcloud.com:15569";
-
+        $server = "583133f034b53.gz.cdb.myqcloud.com";
+        $port = "15569";
         $user = "cdb_outerroot";
         $pwd = "qs^%$#@!";
         //$user = "web";
         //$pwd = "Ot6QzOCnYEhQl4Y2";
         $db = "qgame";
         try{
-            $this->db = new \PDO("mysql:host=$server;dbname=$db", $user, $pwd);
+            $this->db = new \PDO("mysql:host=$server;port=$port;dbname=$db", $user, $pwd);
             $this->db->query("set character set 'utf8'");
         }catch (PDOException $e){
               echo $this->db . "<br>" . $e->getMessage();
